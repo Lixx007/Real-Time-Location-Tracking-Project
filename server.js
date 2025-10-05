fbd2a82876be967f61325e595a217cd0b1e66e7f
suhas pashte <146187296+Lixx007@users.noreply.github.com>
2025-10-05 20:02:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+server.listen(3000, () => console.log("✅ Server running on http://localhost:3000"));
