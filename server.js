8cab1bfb4de5327a5b3988f68b9278d93d1fe9be
suhas pashte <146187296+Lixx007@users.noreply.github.com>
2026-07-31 09:55:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => console.log("✅ Server running on http://localhost:3000"));
+server.listen(3000, () => console.log("Server running on http://localhost:3000"));
